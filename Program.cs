65d3ced19a0aac00211d652017277ad753e0cc72
Olesya <olesyaspb.ru@mail.ru>
2023-05-28 20:43:59 +0300

    написаны решение 3 задания

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ PrintResult(resultLine);
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-
+/*
  int GetSum(int m, int n)
 {
     if (n > m)
@@ -47,7 +47,6 @@ PrintResult(resultLine);
     return m;
 }
 
-// Чтение данных из консоли
 int ReadData(string line)
 {
     Console.Write(line);
@@ -55,25 +54,53 @@ int ReadData(string line)
     return number;
 }
 
-// Метод выводит результат пользователю
 void PrintData(string msg, int res)
 {
     Console.WriteLine(msg + res);
 }
 
-// Основной код программы
-// 1  Вводим данные
 int m = ReadData("Введите число M: ");
 int n = ReadData("Введите число N: ");
 
-// 2 Считаем сумму
 int sum = GetSum(m,n);
 
-// Выводим результат
 PrintData("Сумма элементов от М до N равна: ",sum);
 
-
+*/
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int Ackerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Ackerman(n - 1, 1);
+    else
+      return Ackerman(n - 1, Ackerman(n, m - 1));
+}
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void PrintData(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+
+
+int n = ReadData("Введите число N: ");
+int m = ReadData("Введите число M: ");
+
+int sumAckerman = Ackerman(n, m);
+
+PrintData("значение функции Аккермана равно: ", sumAckerman);
+
+
